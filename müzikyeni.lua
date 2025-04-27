8c245609628280b93443b85575b6291fc3449580
mstfaep <esmustafa07@gmail.com>
2025-04-27 20:41:13 +0300

    Update müzikyeni.lua

--- müzikyeni.lua
+++ müzikyeni.lua
@@ -1,7 +1,7 @@
 for i, v in pairs(game:GetDescendants()) do
-if v.ClassName == "RemoteEvent" and v.Name == "AC6_FE_Sounds" then
+if v.ClassName == "RemoteEvent" and v.Name == "AC6_Sounds" then
 local remote = v
-remote:FireServer("newSound", "", workspace, "rbxassetid://96044784019038", 1, 5000, true)
+remote:FireServer("newSound", "", workspace, "rbxassetid://1839246711", 1, 5000, true)
 remote:FireServer("playSound", "")
 end
 end 
